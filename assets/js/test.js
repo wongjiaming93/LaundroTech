7bddb0834f6574afa10337302805602dc6c5f121
wongjiaming93 <44884351+wongjiaming93@users.noreply.github.com>
2021-04-10 17:23:40 +0800

    Add files via upload

---
+++ assets/js/test.js
@@ -0,0 +1,20 @@
+var callAPI = (firstName,lastName)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("YOUR-API-INVOKE-URL", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
